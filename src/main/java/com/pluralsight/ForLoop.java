b410cd2d4841d7cf76b7aec4d2dc9db0653aac0d
Tayshia McCray <tmccray@my.yearupunited.org>
2025-10-02 10:47:52 -0400

    Initial Commit

--- src/main/java/com/pluralsight/ForLoop.java
+++ src/main/java/com/pluralsight/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
     public static void main(String[] args) throws InterruptedException {
         int count = 0;
         for (int i = 10; i > -1; i--) {
-            System.out.println("Countdown: " +i);
+            System.out.println("Happy New Year: " +i);
             Thread.sleep(1000);
         }
         //After loop ends
