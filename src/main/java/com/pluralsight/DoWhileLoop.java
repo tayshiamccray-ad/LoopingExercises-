180268759429db59bf1a8d338655d33cf27e329a
Tayshia McCray <tmccray@my.yearupunited.org>
2025-10-02 10:46:56 -0400

    Initial Commit

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        do {
+            System.out.println("I love Java!!!");
+            count += 1;
+
+        } while(count < 5);
+    }
+}
